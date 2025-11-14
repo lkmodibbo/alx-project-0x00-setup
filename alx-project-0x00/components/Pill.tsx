f5623eb1834957a5d701405616cd6faa58769755
lkmodibbo <lukmansaidmodibbo@gmail.com>
2025-11-14 16:31:14 +0100

    Pill and Card are created

---
+++ alx-project-0x00/components/Pill.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+const Pill: React.FC<{ title?: string }> = ({ title = "Title"}) => {
+    return (
+        <div className="flex justify-center items-center bg-[#f9f9f9] px-3 w-auto h-[27px] rounded-full">
+            <p className="text-sm">{title}</p>
+        </div>
+    );
+};
+export default Pill;
