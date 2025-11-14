6ccae09571c1a1ba2ff6584b4c40dc21bdcc566d
lkmodibbo <lukmansaidmodibbo@gmail.com>
2025-11-14 17:06:15 +0100

    reusable buttons created

--- alx-project-0x00/pages/landing.tsx
+++ alx-project-0x00/pages/landing.tsx
@@ -1,12 +1,18 @@
-import Card from "@/components/Card"
+import React from "react";
+import Button from "@/components/Button";
 
-const Landing: React.FC =  () => {
+const Landing: React.FC = () => {
   return (
-    <div>
-      <h1 className=" text-xl font-extralight">Landing Page
-      </h1>
-      <Card />
+    <div className="p-8">
+      <h1 className="text-xl font-extralight mb-4">Landing Page</h1>
+
+      <div className="flex gap-4 mb-4">
+        <Button title="Small Rounded" styles="text-sm rounded-sm" />
+        <Button title="Medium Rounded" styles="text-base rounded-md" />
+        <Button title="Large Rounded" styles="text-lg rounded-full" />
+      </div>
     </div>
-  )
-}
-export default Landing
+  );
+};
+
+export default Landing;
